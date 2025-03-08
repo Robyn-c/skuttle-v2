f2f698ab49358847e0b456c5100d9c5ba12c64cc
Robyn <scuadra02@gmail.com>
2025-03-08 15:59:05 -0300

    feat: increased limit from 300 to 750

--- commands/silly/yuri.js
+++ commands/silly/yuri.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('Sends a yuri picture back!'),
 	async execute(interaction) {
 		await interaction.reply('Thinking...');
-		const yuriResult = await request('https://safebooru.org/index.php?page=dapi&s=post&q=index&limit=300&tags=yuri&json=1');
+		const yuriResult = await request('https://safebooru.org/index.php?page=dapi&s=post&q=index&limit=750&tags=yuri&json=1');
 		const yuriList = await yuriResult.body.json();
 		randomNumber = Math.floor(Math.random() * yuriList.length - 1);
 		const { file_url } = yuriList[randomNumber];
